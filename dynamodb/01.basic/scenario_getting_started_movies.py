4a61eb17d97f7f4af0de2ff4452a2750ac9aff0b
seoyeong98 <youngseogimkk@gmail.com>
2024-01-24 13:49:26 +0900

    write_batch

--- dynamodb/01.basic/scenario_getting_started_movies.py
+++ dynamodb/01.basic/scenario_getting_started_movies.py
@@ -14,8 +14,9 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger(__name__)
 
 class Movies:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dyn_resource) -> None:
+        self.dyn_resource = dyn_resource
+        self.table=None
 
     def exists(self, table_name):
         pass
@@ -27,7 +28,25 @@ class Movies:
         pass
 
     def write_batch(self, movies):
-        pass
+        """
+        Table.batch_writer() 함수로 테이블에 items 를 put 하는 메소드
+        - Inside the context manager, Table.batch_writer builds a list of requests.
+        - context manager에 구현되어있는 매직메소드를 사용해서 put_item()을 했을 때
+            DynamoDB로 batches of write requests를 보내고, 
+            이 과정에서의 chunking, buffering, retrying은 자동으로 수행된다.
+        """
+        try:
+            with self.table.batch_writer() as writer:
+                for movie in movies:
+                    writer.put_item(Item=movie)
+        except ClientError as err:
+            logger.error(
+                "Couln't load data into table %s. Here's why: %s %s",
+                self.table.name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
 
     def add_movies(self, title, year, plot, rating):
         pass
@@ -47,6 +66,11 @@ class Movies:
     def delete_movie(self, title, year):
         pass
 
+def run_scenario(table_name, movie_file_name, dyn_resources):
+    logging.basicConfig(level=logging.INFO, 
+                        format=f"%(levelname)s: %(message)s")
+    
+    movies = Movies(dyn_resources)
 
 if __name__ == "__main__":
     pass
